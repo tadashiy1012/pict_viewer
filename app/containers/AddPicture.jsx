5503d52c769894946e8cd3e5e5443db7d47d91b3
tadashiy1012 <tadashi.y1012@gmail.com>
2016-07-25 14:39:30 +0900

    add async image read func

--- app/containers/AddPicture.jsx
+++ app/containers/AddPicture.jsx
@@ -3,25 +3,66 @@ import { connect } from 'react-redux';
 import { addPicture, clearPictures } from '../actions';
 
 const remote = window.require('electron').remote;
+const ipc = window.require('electron').ipcRenderer;
+let reciverInit = false;
 
-let AddPicture = ({ dispatch }) => {
+const setReciver = (func) => {
+  ipc.on('getImg-reply', (ev, arg) => {
+    func(arg);
+  });
+  reciverInit = true;
+}
+
+const check = (tgt) => {
+  const ext = tgt.substring(tgt.lastIndexOf('.') + 1);
+  if (ext === 'jpg' || ext === 'png') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const addForm = ({onSubmitWithClear, onReciveData}) => {
+  if (!reciverInit) {
+    setReciver(onReciveData);
+  }
   return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault();
-        dispatch(clearPictures());
-        remote.require('./openDir')((arg) => {
-          arg.map((data) => {
-            dispatch(addPicture(data));
-          });
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      onSubmitWithClear();
+      remote.require('./openDir')((arg) => {
+        const files = arg.filter((f) => {
+          return check(f);
         });
-      }}>
-        <button type="submit">Open Directory</button>
-      </form>
-    </div>
+        files.forEach((f) => {
+          ipc.send('getImg', f);
+        });
+      });
+    }}>
+      <button type="submit">Open Directory</button>
+    </form>
   );
 };
 
-AddPicture = connect()(AddPicture);
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSubmitWithClear: () => {
+      dispatch(clearPictures());
+    },
+    onReciveData: (data) => {
+      dispatch(addPicture(data));
+    }
+  };
+};
+
+const AddPicture = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(addForm);
 
 export default AddPicture;
