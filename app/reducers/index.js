dc9f25eb564ce0c566450d9d0ee3f0623101dfd5
tadashiy1012 <tadashi.y1012@gmail.com>
2016-07-28 16:03:08 +0900

    add pagenation func

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,15 +1,22 @@
 import {handleAction, handleActions} from 'redux-actions';
-import {addPicture, clearPictures, showModal, closeModal} from '../actions';
+import {
+  addPicture, clearPictures,
+  showModal, closeModal,
+  setData,
+  setPageSize, setPage, nextPage, prevPage
+} from '../actions';
 
 const reducer = handleActions({
   [addPicture]: (state, action) => {
     return Object.assign({}, state, {
-      pictures: [...state.pictures, action.payload]
+      pictures: [...state.pictures, action.payload],
+      loaded: false 
     });
   },
   [clearPictures]: (state, action) => {
     return Object.assign({}, state, {
-      pictures: []
+      pictures: [],
+      loaded: false 
     });
   },
   [showModal]: (state, action) => {
@@ -24,10 +31,36 @@ const reducer = handleActions({
     return Object.assign({}, state, {
       modal: ''
     });
+  },
+  [setData]: (state, action) => {
+    return Object.assign({}, state, { files: action.payload,
+      loaded: true });
+  },
+  [setPageSize]: (state, action) => {
+    return Object.assign({}, state, { pageSize: action.payload, 
+      loaded: false });
+  },
+  [setPage]: (state, action) => {
+    return Object.assign({}, state, { page: action.payload, 
+      loaded: false });
+  },
+  [nextPage]: (state, action) => {
+    return Object.assign({}, state, {
+      page: state.page < state.pageSize ? state.page + 1 : state.page
+    });
+  },
+  [prevPage]: (state, action) => {
+    return Object.assign({}, state, {
+      page: state.page > 1 ? state.page - 1 : state.page
+    })
   }
 }, {
+  files: [],
   pictures: [],
-  modal: ''
+  modal: '',
+  pageSize: 0,
+  page: 0,
+  loaded: false
 });
 
 export default reducer;
