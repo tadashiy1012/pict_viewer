385d2dc5c6e55f41eeb8fa4f05446b9b3cd836b4
tadashiy1012 <tadashi.y1012@gmail.com>
2016-07-28 16:02:10 +0900

    modify use promise

--- openDir.js
+++ openDir.js
@@ -4,16 +4,16 @@ const browserWindow = electron.BrowserWindow;
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function openDir(callback) {
-  let focusedWindow = browserWindow.getFocusedWindow();
-  dialog.showOpenDialog(focusedWindow, {
-    properties: ['openDirectory']
-  }, (args) => {
-    fs.readdir(args[0], (err, data) => {
-      const paths = data.map((name) => {
-        return path.join(args[0], name);
+module.exports = function openDir() {
+  return new Promise((done) => {
+    let focusedWindow = browserWindow.getFocusedWindow();
+    dialog.showOpenDialog(focusedWindow, {
+      properties: ['openDirectory']
+    }, (args) => {
+      fs.readdir(args[0], (err, data) => {
+        const paths = data.map(name => path.join(args[0], name));
+        done(paths);
       })
-      callback(paths);
-    })
+    });  
   });
 };
